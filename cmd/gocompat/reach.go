daf34f762bcb21e7fb5494f3da0b1befd574d409
Santiago M. Mola <santi@mola.io>
2018-12-03 12:18:55 +0100

    move reachability code to main package
    Signed-off-by: Santiago M. Mola <santi@mola.io>

--- cmd/gocompat/reach.go
+++ cmd/gocompat/reach.go
@@ -2,14 +2,10 @@ package main
 
 import (
 	"fmt"
-	"go/ast"
-	"go/types"
-	"reflect"
 	"sort"
 
-	"golang.org/x/tools/go/loader"
+	compat "github.com/smola/gocompat"
 	"gopkg.in/src-d/go-cli.v0"
-	"gopkg.in/src-d/go-log.v1"
 )
 
 func init() {
@@ -24,31 +20,14 @@ type reachCommand struct {
 }
 
 func (c reachCommand) Execute(args []string) error {
-	var conf loader.Config
-	_, err := conf.FromArgs(c.Positional.Packages, false)
+	reachable, err := compat.ReachableFromPackages(c.Positional.Packages...)
 	if err != nil {
-		log.Errorf(err, "creating load configuration")
 		return err
-	}
-
-	prog, err := conf.Load()
-	if err != nil {
-		log.Errorf(err, "loading packages")
-		return err
-	}
-
-	seen := make(map[string]bool)
-	reachable := make(map[types.Object]bool)
-	for _, pkg := range prog.InitialPackages() {
-		if err := reachFromPackage(seen, reachable, pkg); err != nil {
-			log.Errorf(err, "processing package: %s", pkg.Pkg.Name)
-			return err
-		}
 	}
 
 	var reachedString []string
-	for symbol := range reachable {
-		str := fmt.Sprintf("\"%s\".%s", symbol.Pkg().Path(), symbol.Name())
+	for _, r := range reachable {
+		str := fmt.Sprintf("\"%s\".%s", r.Pkg().Path(), r.Name())
 		reachedString = append(reachedString, str)
 	}
 
@@ -59,91 +38,4 @@ func (c reachCommand) Execute(args []string) error {
 	}
 
 	return nil
-}
-func reachFromPackage(seen map[string]bool, reachable map[types.Object]bool, pkg *loader.PackageInfo) error {
-	if !pkg.TransitivelyErrorFree {
-		return fmt.Errorf("has errors")
-	}
-
-	scope := pkg.Pkg.Scope()
-	for _, name := range scope.Names() {
-		if !ast.IsExported(name) {
-			continue
-		}
-
-		obj := scope.Lookup(name)
-		reachFromObject(seen, reachable, pkg.Pkg, obj)
-	}
-
-	return nil
-}
-
-func reachFromObject(seen map[string]bool, reachable map[types.Object]bool, pkg *types.Package, obj types.Object) {
-	if reachable[obj] {
-		return
-	}
-	reachable[obj] = true
-
-	if !obj.Exported() {
-		return
-	}
-
-	reachFromType(seen, reachable, pkg, obj.Type())
-}
-
-func reachFromType(seen map[string]bool, reachable map[types.Object]bool, pkg *types.Package, typ types.Type) {
-	if seen[typ.String()] {
-		return
-	}
-	seen[typ.String()] = true
-
-	switch typ := typ.(type) {
-	case *types.Named:
-		obj := typ.Obj()
-		opkg := obj.Pkg()
-		if opkg == nil {
-			return
-		}
-
-		if opkg != pkg {
-			reachFromObject(seen, reachable, opkg, obj)
-			return
-		}
-
-		if !typ.Obj().Exported() {
-			return
-		}
-
-		for i := 0; i < typ.NumMethods(); i++ {
-			m := typ.Method(i)
-			reachFromType(seen, reachable, pkg, m.Type())
-		}
-
-	case *types.Pointer:
-		reachFromType(seen, reachable, pkg, typ.Elem())
-	case *types.Struct:
-		for i := 0; i < typ.NumFields(); i++ {
-			f := typ.Field(i)
-			if !f.Exported() {
-				continue
-			}
-			reachFromType(seen, reachable, pkg, f.Type())
-		}
-	case *types.Signature:
-		reachFromType(seen, reachable, pkg, typ.Params())
-		reachFromType(seen, reachable, pkg, typ.Results())
-	case *types.Tuple:
-		for i := 0; i < typ.Len(); i++ {
-			f := typ.At(i)
-			reachFromType(seen, reachable, pkg, f.Type())
-		}
-	case *types.Slice:
-		reachFromType(seen, reachable, pkg, typ.Elem())
-	case *types.Map:
-		reachFromType(seen, reachable, pkg, typ.Key())
-		reachFromType(seen, reachable, pkg, typ.Elem())
-	case *types.Basic:
-	default:
-		log.Warningf("unhandled type: %f", reflect.TypeOf(typ))
-	}
 }
---
+++ reach.go
@@ -0,0 +1,174 @@
+package compat
+
+import (
+	"fmt"
+	"go/ast"
+	"go/types"
+	"reflect"
+
+	"golang.org/x/tools/go/loader"
+	"gopkg.in/src-d/go-log.v1"
+)
+
+// ReachableFromPackages gets all top-level exported types.Object reachable from
+// the given list of package paths.
+func ReachableFromPackages(pkgs ...string) ([]types.Object, error) {
+	r := newReachability()
+	if err := r.FromPackages(pkgs...); err != nil {
+		return nil, err
+	}
+
+	return r.GetReachable(), nil
+}
+
+type reachability struct {
+	seen      map[types.Type]bool
+	reachable map[types.Object]bool
+}
+
+func newReachability() *reachability {
+	return &reachability{
+		seen:      make(map[types.Type]bool),
+		reachable: make(map[types.Object]bool),
+	}
+}
+
+func (r *reachability) GetReachable() []types.Object {
+	result := make([]types.Object, 0, len(r.reachable))
+	for obj := range r.reachable {
+		result = append(result, obj)
+	}
+
+	return result
+}
+
+func (r *reachability) FromPackages(pkgs ...string) error {
+	var conf loader.Config
+	_, err := conf.FromArgs(pkgs, false)
+	if err != nil {
+		log.Errorf(err, "creating load configuration")
+		return err
+	}
+
+	prog, err := conf.Load()
+	if err != nil {
+		log.Errorf(err, "loading packages")
+		return err
+	}
+
+	for _, pkg := range prog.InitialPackages() {
+		if err := r.fromPackage(pkg); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (r *reachability) fromPackage(pkg *loader.PackageInfo) error {
+	if !pkg.TransitivelyErrorFree {
+		return fmt.Errorf("has errors")
+	}
+
+	scope := pkg.Pkg.Scope()
+	for _, name := range scope.Names() {
+		if !ast.IsExported(name) {
+			continue
+		}
+
+		obj := scope.Lookup(name)
+		if err := r.reachFromObject(pkg.Pkg, obj); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (r *reachability) reachFromObject(pkg *types.Package, obj types.Object) error {
+	if r.reachable[obj] {
+		return nil
+	}
+	r.reachable[obj] = true
+
+	if !obj.Exported() {
+		return nil
+	}
+
+	return r.reachFromType(pkg, obj.Type())
+}
+
+func (r *reachability) reachFromType(pkg *types.Package, typ types.Type) error {
+	if r.seen[typ] {
+		return nil
+	}
+	r.seen[typ] = true
+
+	switch typ := typ.(type) {
+	case *types.Named:
+		obj := typ.Obj()
+		opkg := obj.Pkg()
+		if opkg == nil {
+			return nil
+		}
+
+		if opkg != pkg {
+			return r.reachFromObject(opkg, obj)
+		}
+
+		if !typ.Obj().Exported() {
+			return nil
+		}
+
+		for i := 0; i < typ.NumMethods(); i++ {
+			m := typ.Method(i)
+			if err := r.reachFromType(pkg, m.Type()); err != nil {
+				return err
+			}
+		}
+
+		return nil
+	case *types.Pointer:
+		return r.reachFromType(pkg, typ.Elem())
+	case *types.Struct:
+		for i := 0; i < typ.NumFields(); i++ {
+			f := typ.Field(i)
+			if !f.Exported() {
+				continue
+			}
+
+			if err := r.reachFromType(pkg, f.Type()); err != nil {
+				return err
+			}
+		}
+
+		return nil
+	case *types.Signature:
+		if err := r.reachFromType(pkg, typ.Params()); err != nil {
+			return err
+		}
+
+		return r.reachFromType(pkg, typ.Results())
+	case *types.Tuple:
+		for i := 0; i < typ.Len(); i++ {
+			f := typ.At(i)
+			if err := r.reachFromType(pkg, f.Type()); err != nil {
+				return err
+			}
+		}
+
+		return nil
+	case *types.Slice:
+		return r.reachFromType(pkg, typ.Elem())
+	case *types.Map:
+		if err := r.reachFromType(pkg, typ.Key()); err != nil {
+			return err
+		}
+
+		return r.reachFromType(pkg, typ.Elem())
+	case *types.Basic:
+		return nil
+	default:
+		return fmt.Errorf("unhandled type: %f", reflect.TypeOf(typ))
+	}
+}
